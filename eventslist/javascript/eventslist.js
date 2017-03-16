b08c9f40befae60763c0422c114ff69c21517f75
Ben Martin <ben.martin@udsu.co.uk>
2017-03-16 17:30:52 +0000

    Added time calc and orgid attributes

--- eventslist/javascript/eventslist.js
+++ eventslist/javascript/eventslist.js
@@ -52,25 +52,75 @@ function getTimefromMSL(time) {
 	return [hh,mm,0]
 }
 
+function timeAgo(time){
+
+	switch (typeof time) {
+		case 'number': break
+		case 'string': time = +new Date(time); break
+		case 'object': if (time.constructor === Date) time = time.getTime(); break
+		default: time = +new Date()
+	}
+
+	var time_formats = [
+    [60, 'seconds', 1],
+    [120, '1 minute ago', '1 minute from now'],
+    [3600, 'minutes', 60],
+    [7200, '1 hour ago', '1 hour from now'],
+    [86400, 'hours', 3600],
+    [172800, 'Yesterday', 'Tomorrow'],
+    [604800, 'days', 86400],
+    [1209600, 'Last week', 'Next week'],
+    [2419200, 'weeks', 604800],
+    [4838400, 'Last month', 'Next month'],
+    [29030400, 'months', 2419200],
+    [58060800, 'Last year', 'Next year'],
+    [2903040000, 'years', 29030400],
+    [5806080000, 'Last century', 'Next century'],
+    [58060800000, 'centuries', 2903040000]
+    ]
+
+    var seconds = (+new Date() - time) / 1000,
+    token = 'ago', list_choice = 1
+
+    if (seconds == 0) {
+    	return 'Just now'
+    }
+    if (seconds < 0) {
+    	seconds = Math.abs(seconds);
+    	token = 'from now'
+    	list_choice = 2
+    }
+    var i = 0, format;
+    while (format = time_formats[i++])
+    	if (seconds < format[0]) {
+    		if (typeof format[2] == 'string')
+    			return format[list_choice]
+    		else
+    			return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token
+    	}
+    return time
+}
+
 function reformatEvents(eventlist) {
 	let html = ''
+
 	eventlist.forEach((event) => {
-		html += '<div class="eventlist-item">'+
-		'<a class="eventlist-item__link" href="#">'+
-		'<div class="eventlist-item__image" style="background-image: url('+event.image+')"><div></div></div>'+
-		'<div class="eventlist-item__content">'+
-		'<div class="eventlist-item__title">'+
-		event.title+
-		'</div>'+
-		'<div class="eventlist-item__lead">'+
-		event.lead+
-		'</div>'+
-		'<div class="eventlist-item__meta">'+
-		'<p>Posted <time datetime="" title="">'+event.date+'</time></p>'+
-		'</div>'+
-		'</div>'+
-		'</a>'+
-		'</div>'
+		html += '<div class="eventlist-item" data-orgid="'+event.org[0]+'">'+
+				'<a class="eventlist-item__link" href="#">'+
+					'<div class="eventlist-item__image" style="background-image: url('+event.image+')"><div></div></div>'+
+					'<div class="eventlist-item__content">'+
+						'<div class="eventlist-item__title">'+
+							event.title+
+						'</div>'+
+						'<div class="eventlist-item__lead">'+
+							event.lead+
+						'</div>'+
+						'<div class="eventlist-item__meta">'+
+							'<p>happening <time datetime="'+event.date+'" title="'+event.date+'">'+timeAgo(event.date)+'</time></p>'+
+						'</div>'+
+					'</div>'+
+				'</a>'+
+			'</div>'
 	})
 	document.querySelector(".msl_eventlist").innerHTML = html
 }
